2436471ff4b3498289addf9b76e97de8206d5e88
ankitjain212003 <119718599+ankitjain212003@users.noreply.github.com>
2025-04-15 12:06:51 +0530

    banner removed

--- APTRS/wsgi.py
+++ APTRS/wsgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from .init import current_version
 
 
-BANNER, COPYRIGHT = current_version()
+
+
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APTRS.settings')
 application = get_wsgi_application()
-print(BANNER+ COPYRIGHT)
+

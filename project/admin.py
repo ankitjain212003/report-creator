8ba1b23d23a904ae47c2f22b980549383f31261e
ankitjain212003 <119718599+ankitjain212003@users.noreply.github.com>
2025-04-17 12:06:40 +0530

    sdfgh

--- project/admin.py
+++ project/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Project,Vulnerability, Vulnerableinstance
+from .models import Project,Vulnerability, Vulnerableinstance,WorkOrder
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Vulnerability)
 admin.site.register(Vulnerableinstance)
+admin.site.register(WorkOrder)
